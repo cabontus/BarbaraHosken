1364a86384300c769abc1383e499d3854ebe55a2
Carla Bontus <ccobontus@gmail.com>
2017-06-12 09:02:26 -0300

    Criação do arquivo site.js linkado no HTML contendo todas as chamadas de js dentro dele

---
+++ site/js/site.js
@@ -0,0 +1,36 @@
+////////////////////////////////////////////////////
+//Animated section library
+////////////////////////////////////////////////////
+AOS.init({
+});
+
+////////////////////////////////////////////////////
+//Scroll to each section by menu click
+////////////////////////////////////////////////////
+$('[href="#evaluation"]').click(function(){
+	$('#evaluation').animatescroll({
+		scrollSpeed:1000,
+		padding: "56"
+	});
+});
+
+$('[href="#aboutUs"]').click(function(){
+	$('#aboutUs').animatescroll({
+		scrollSpeed:1000,
+		padding: "120"
+	});
+});
+
+$('[href="#therapies"]').click(function(){
+	$('#therapies').animatescroll({
+		scrollSpeed:1000,
+		padding: "56"
+	});
+});
+
+$('[href="#mainFooter"]').click(function(){
+	$('#mainFooter').animatescroll({
+		scrollSpeed:1000,
+		padding: "58"
+	});
+});
